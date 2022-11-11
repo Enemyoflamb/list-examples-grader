9f14e69531b240bf08a2369cab4caeb094bc4a67
Brendan L Kuang <blkuang@UCSD.EDU>
2022-11-10 17:23:39 -0800

    up to grader implemented

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,30 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.*;
 
-public class TestListExamples {
-  // Write your grading tests here!
+public class TestListExamples{
+  
+  class len5sc implements StringChecker{
+        public boolean checkString(String s){
+            return s.length()>5;
+        }
+    }
+    @Test
+    public void scEmpty(){
+        assertEquals(new ArrayList<>(), ListExamples.filter(new ArrayList<String>(), new len5sc() ));
+    }
+    @Test
+    public void oneSc(){
+        ArrayList a = new ArrayList<String>();
+        a.add("111111");
+        a.add( "abcdef");
+        assertEquals(a, ListExamples.filter(a, new len5sc() ));
+    }
+    @Test
+    public void basicMerge(){
+        ArrayList a = new ArrayList<String>(Arrays.asList("a","c","e"));
+        ArrayList b = new ArrayList<String>(Arrays.asList("b", "z"));
+        assertEquals(new ArrayList<String>(Arrays.asList("a","b","c","e", "z")), ListExamples.merge(a, b));
+    }
+
 }
--- grade.sh
+++ grade.sh
@@ -4,4 +4,9 @@ set -e
 
 rm -rf student-submission
 git clone $1 student-submission
-
+CPATH=.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar
+cp -R ./lib ./student-submission
+cp *.java ./student-submission
+cd student-submission
+javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" *.java
+java -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" org.junit.runner.JUnitCore TestListExamples
